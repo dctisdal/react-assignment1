8446993f49686c79a5f0d001a647149075fbe1af
dctisdal <dctisdal@calpoly.edu>
2020-04-11 11:35:39 +0700

    mod app and table for delete functionality

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
-    render() {
-      const characters = [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
         },
         {
           name: 'Mac',
@@ -19,15 +19,33 @@ class App extends Component {
         {
           name: 'Dennis',
           job: 'Bartender',
-        },
-      ]
-  
-      return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      )
-    }
+      }
+    ],
   }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
+    const { characters } = this.state
+  
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+      </div>
+    )
+  }
+}
+
+
+
+
 
 export default App
